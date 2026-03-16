22c941efadc4ade0f5d4914c372e292ed2e5fa61
TanujaModugumudi <mtanuja1804@gmail.com>
2026-03-16 14:12:26 +0530

    Update API URL and CORS for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'https://querymind-backend-kzd8.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
